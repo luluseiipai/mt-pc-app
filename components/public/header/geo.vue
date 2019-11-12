bbc7a32565b69a4818048206ee93eae5f2e9e806
NiKQ <780366011@qq.com>
2019-11-12 17:49:15 +0800

    first commit

---
+++ components/public/header/geo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" />北京市
+    <nuxt-link class="changeCity" to="/changeCity">
+      切换城市
+    </nuxt-link>
+    [暗示 负担 沙发]
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModuleGeo'
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row class="m-header">
+    <el-col :span="4">
+      <geo />
+    </el-col>
+    <el-col :span="6">
+      <user />
+    </el-col>
+    <el-col :span="14">
+      <div class="grid-content bg-purple" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo'
+import User from './user'
+export default {
+  components: {
+    Geo,
+    User
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/public/layout.scss';
+</style>
